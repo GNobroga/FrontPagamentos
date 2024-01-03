540bd65ec02642d4bbbf62b75f7f487e10b65298
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-01-03 13:11:54 -0300

    Implementação do componente months

--- src/app/modules/fintech/components/months/months.component.ts
+++ src/app/modules/fintech/components/months/months.component.ts
@@ -1,4 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output, signal } from '@angular/core';
+
+const months = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+];
 
 @Component({
   selector: 'app-months',
@@ -6,6 +21,33 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './months.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MonthsComponent {
+export class MonthsComponent implements OnInit {
 
+  public data = signal([]);
+
+  public selectMonths = signal<{ month: string, key: number }[]>([]);
+
+  @Output() public selectedMonth = new EventEmitter<number>();
+
+  public ngOnInit(): void {
+      this.#getLastMonths(4);
+  }
+
+  #getLastMonths(count: number) {
+    const currentDate = new Date();
+    currentDate.setMonth(-count + 1);
+    let currentMonth = -count + 1;
+
+    for (let i = 0 ; i < count ; i++) {
+      const monthKey = currentDate.getMonth();
+      const month = { month: months.at(monthKey) as string, key: monthKey };
+      this.selectMonths.update((oldValue) => [...oldValue, month]);
+      currentMonth += 1;
+      currentDate.setMonth(currentMonth);
+    }
+  }
+
+  public onSelectedMonth(month: number) {
+    this.selectedMonth.emit(month);
+  }
 }
